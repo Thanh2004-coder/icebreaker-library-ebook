74527146d05d2c2730803951fc866a5b44b751aa
Thanh2004-coder <thanh200417@gmail.com>
2026-08-23 14:36:23 +0700

    1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
-import GamePage from "./pages/GamePage.jsx";
 import Footer from "./components/Footer.jsx";
 
 export default function App() {
-  return (
-    <div className="app-shell">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/page/:page" element={<HomePage />} />
-        <Route path="/games/:id" element={<GamePage />} />
-        <Route path="*" element={<Navigate to="/page/1" replace />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="app-shell">
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/page/:page" element={<HomePage />} />
+                <Route
+                    path="/games/:id"
+                    element={<Navigate to="/page/1" replace />}
+                />
+                <Route
+                    path="*"
+                    element={<Navigate to="/page/1" replace />}
+                />
+            </Routes>
+
+            <Footer />
+        </div>
+    );
 }
